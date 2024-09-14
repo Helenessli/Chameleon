636f44206ba79171258699d3d9a4af36aa33da3d
Helenessli <helenessli6@gmail.com>
2024-09-14 11:53:43 -0400

    add api call

---
+++ api/api.py
@@ -0,0 +1,55 @@
+import base64
+import requests
+import os
+from dotenv import load_dotenv
+import openai  
+
+# Load environment variables
+load_dotenv()
+
+# Access OpenAI API key
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+current_directory = os.path.dirname(__file__)
+image_path = os.path.join(current_directory, "bird.jpg")
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o-mini",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
+
